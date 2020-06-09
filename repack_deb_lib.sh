dda9fad7e1fe4317786c236a84316ebb10cd3bd6
Joo Aun Saw <jasaw@dius.com.au>
2020-06-09 21:42:37 +1000

    repack script: fix missing libcudnn dev file

--- repack_deb_lib.sh
+++ repack_deb_lib.sh
@@ -28,24 +28,27 @@ unpack_deb()
 	fi
 }
 
+# libcudnn rt
 mkdir -p output/libcudnn-${LIBCUDNN_VERSION}/rt
 cd output/libcudnn-${LIBCUDNN_VERSION}/rt
 for f in ${TARGET_DNN_RT_DEB_FILES}
 do
 	unpack_deb "../../../dl/$f" 1
 done
-rm -f control.tar.gz data.tar.xz debian-binary _gpgbuilder
+rm -f control.tar.xz control.tar.gz data.tar.xz debian-binary _gpgbuilder
 cd ../../..
 
+# libcudnn dev
 mkdir -p output/libcudnn-${LIBCUDNN_VERSION}/dev
 cd output/libcudnn-${LIBCUDNN_VERSION}/dev
-for f in ${TARGET_DNN_DEB_FILES}
+for f in ${TARGET_DNN_DEV_DEB_FILES}
 do
 	unpack_deb "../../../dl/$f" 1
 done
-rm -f control.tar.gz data.tar.xz debian-binary _gpgbuilder
+rm -f control.tar.xz control.tar.gz data.tar.xz debian-binary _gpgbuilder
 cd ../../..
 
+# CUDA aarch64
 mkdir -p output/tmp_target
 cd output/tmp_target
 for f in ${TARGET_DEB_FILES}
@@ -56,9 +59,12 @@ cd ../..
 mkdir output/cuda-${CUDA_VERSION}-tegra210
 cd output/cuda-${CUDA_VERSION}-tegra210
 find ../tmp_target -type f -name "*.deb" | xargs -I % sh -c 'echo "Extracting %" ; ar x "%" ; tar xf data.tar.xz '
-rm -f control.tar.gz data.tar.xz debian-binary _gpgbuilder
+rm -f control.tar.xz control.tar.gz data.tar.xz debian-binary _gpgbuilder
+rm -rf usr/share/doc usr/local/cuda-${CUDA_VERSION}/tools usr/local/cuda-${CUDA_VERSION}/doc usr/local/cuda-${CUDA_VERSION}/samples
+rm -rf usr/local/cuda-${CUDA_VERSION}/nvvm/libnvvm-samples usr/local/cuda-${CUDA_VERSION}/nvml/example usr/local/cuda-${CUDA_VERSION}/extras/CUPTI/sample
 cd ../..
 
+# CUDA ubuntu1804
 mkdir -p output/tmp_host
 cd output/tmp_host
 for f in ${HOST_DEB_FILES}
@@ -69,10 +75,10 @@ cd ../..
 mkdir output/cuda-${CUDA_VERSION}-${HOST_OS}
 cd output/cuda-${CUDA_VERSION}-${HOST_OS}
 find ../tmp_host -type f -name "*.deb" | xargs -I % sh -c 'echo "Extracting %" ; ar x "%" ; tar xf data.tar.xz '
-rm -f control.tar.gz data.tar.xz debian-binary _gpgbuilder
+rm -f control.tar.xz control.tar.gz data.tar.xz debian-binary _gpgbuilder
 cd ../..
 
-
+# Compress
 cd output
 echo "Compressing tegra210 CUDA files to output/cuda-${CUDA_VERSION}-tegra210.tar.xz"
 XZ_OPT=-9 tar cfJ cuda-${CUDA_VERSION}-tegra210.tar.xz cuda-${CUDA_VERSION}-tegra210
